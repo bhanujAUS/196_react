d689e33b4101b8d8000149f6a3b138ed16c75d25
bhanuj <b.bawdhane@accenture.com>
2023-09-01 11:31:05 +1000

    Inital Commit

--- src/components/storyboard/index.js
+++ src/components/storyboard/index.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { animatedCards, poster } from '../../constants';
+import {staggerContainer, slideIn} from '../../utils/motion'
 
 export const StoryBaord = () => {
   return (
-    <div>StoryBoard</div>
+    <section>
+      <div className="storyboard-container">
+        {animatedCards.map( (image, index) => (
+          <motion.div
+            variants={staggerContainer}
+            initial='hidden'
+            whileInView="show"
+            className='story-container'
+            >
+              <motion.div
+                variants={slideIn('right', 'tween', 0.2, 1)}
+                className='story'
+              >
+                <img
+                  src={image.imgUrl}
+                  alt={image.id}
+                  className=''
+                />
+                <h3 className='story-text'>
+                    {image.title}
+                </h3>
+              </motion.div>
+          </motion.div>
+        ))}
+
+        {/* Poster */}
+        <motion.div
+            variants={staggerContainer}
+            initial='hidden'
+            whileInView="show"
+            className='story-container'
+            >
+              <motion.div
+                variants={slideIn('right', 'tween', 0.2, 1)}
+                className='story'
+              >
+                <img
+                  src={poster.imgUrl}
+                  alt={poster.id}
+                  className=''
+                />
+                <a className='story-poster-text' href='/'>
+                    {poster.title}
+                </a>
+              </motion.div>
+          </motion.div>
+      </div>
+    </section>
   )
 }
---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+export const animatedCards = [
+    {
+        id: 'whole',
+        imgUrl: '/1.jpeg',
+        title: 'WHOLE',
+    },
+    {
+        id: 'freeze',
+        imgUrl: '/2.jpeg',
+        title: 'FREEZE',
+    },
+    {
+        id: 'crush',
+        imgUrl: '/3.png',
+        title: 'CRUSH',
+    },
+    {
+        id: 'interim',
+        imgUrl: '/4.png',
+        title: 'MINUS 196',
+    },
+];
+
+export const poster = {
+    id: 'poster',
+    imgUrl: '/final.png',
+    title: 'CLICK HERE TO CLAIM',
+}
