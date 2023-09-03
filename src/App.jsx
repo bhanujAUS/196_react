7ecb9a6690bdd96a0fc581c5f458c72d48599f51
He <katharine.he@accenture.com>
2023-09-04 08:53:26 +1000

    background and effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css';
 import StoryBoardSlider from './components/slider/ImageSlider';
+import ShiningEffect from './components/shine'
+//import BackgroundImageAnimation from './components/background';
+
+import React from 'react';
 
 function App() {
+
+  const containerStyle = {
+    backgroundImage: `url(${'/background.PNG'})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <StoryBoardSlider></StoryBoardSlider>
-      </header>
+    <div style={ containerStyle }>
+ 
+      <ShiningEffect></ShiningEffect>      
+      <StoryBoardSlider></StoryBoardSlider>
+            
     </div>
   );
 }
---
+++ src/components/shine/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import './styles.css';
+
+const ShiningEffect = () => {
+    const controls = useAnimation();
+  
+    React.useEffect(() => {
+      controls.start({
+        opacity: 1, 
+        width: '100%', 
+        height: '100%', 
+        x: '-50%', 
+        y: '-50%', 
+        transition: {
+          duration: 10, 
+          repeat: Infinity
+        },
+      });
+    }, [controls]);
+  
+    return (
+      <div className="shining-container">
+        <motion.div
+          className="shine"
+          initial={{ opacity: 0, width: '0%', height: '0%', x: '0%', y: '0%' }}
+          animate={controls}
+        />
+      </div>
+    );
+  };
+
+export default ShiningEffect;
