f7c538849b29dfb239561af46562fbc63f0fea29
bhanuj <b.bawdhane@accenture.com>
2023-09-04 16:40:20 +1000

    Added changes

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -8,25 +8,9 @@ export const staggerContainer = (staggerChildren, delayChildren) => ({
     },
 });
 
-// export const slideIn = (direction, type, delay, duration) => ({
-//     hidden: {
-//         x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0
-//     },
-//     show: {
-//         x: 0,
-//         y: 0,
-//         opacity: 1,
-//         transition: {
-//             type,
-//             delay,
-//             duration,
-//             ease: 'easeOut',
-//         },
-//     },
-// });
-
 export const slideVariants = (start, end) => ({
     initial: { x: start === 'left' ? '-100%' : start === 'right' ? '100%' : 0 },
     animate: { x: end === 'left' ? '-100vh' : end === 'right' ? '100vh' : 0 , transition: { duration: 3.5 } },
     exit: { x: '-100vh', opacity: 0, transition: { duration: 10, ease: 'easeInOut' } },
+    final: { opacity: 1}
 });
