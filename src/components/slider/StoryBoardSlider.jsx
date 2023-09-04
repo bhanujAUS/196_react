bb20432ac5e1084c6d116dc8a59622982a2a280b
bhanuj <b.bawdhane@accenture.com>
2023-09-04 15:46:36 +1000

    Final Variant

--- src/components/slider/StoryBoardSlider.jsx
+++ src/components/slider/StoryBoardSlider.jsx
@@ -6,41 +6,62 @@ import { slideVariants, staggerContainer } from '../../utils/motion';
 
 const StoryBoardSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  
+  const [stopRotation, setStopRotation] = useState(false); // Add a state to control rotation
+
+  const finalVariant = {
+    opacity: 1
+  };
+
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % animatedCards.length);
+      if (currentIndex < animatedCards.length - 1) {
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      } else {
+        // Stop the rotation when we reach the last image with id "poster"
+        clearInterval(timer);
+        setStopRotation(true);
+      }
     }, 2000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [currentIndex, stopRotation]);
 
   return (
     <section className='slider-container'>
       <AnimatePresence>
         <motion.div
+          key={'test'}
           variants={staggerContainer}
           initial='hidden'
           whileInView='show'
-          viewport={{once: true}}
-        >   
+          viewport={{ once: true }}
+        >
           {animatedCards.map((image, index) => (
-            <div>
+            <div key={image.id}>
               <motion.img
                 className='slider-image'
-                key={image.id}
                 src={image.imgUrl}
                 alt={image.id}
                 variants={slideVariants('right', 'left')}
                 initial='initial'
-                animate={index === currentIndex ? 'animate' : ''}
-                exit='exit'                
+                // Use a conditional to determine the animation for the last image
+                animate={
+                  index === currentIndex
+                    ? stopRotation
+                      ? finalVariant // Apply the "final" variant to keep it static
+                      : 'animate' // Continue with the regular animation
+                    : ''
+                }
+                exit='exit'
               />
-                {/* <div>
-                    <a className='story-poster-text' href='/'>
-                        {image.title}
-                    </a>
-                </div> */}
+              {/* Check if the current image is the last one with id "poster" */}
+              {/* {index === animatedCards.length - 1 && image.id === 'poster' && (
+                <div>
+                  <a className='story-poster-text' href='/'>
+                    {image.title}
+                  </a>
+                </div>
+              )} */}
             </div>
           ))}
         </motion.div>
