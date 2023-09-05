403962689971059cd712935112b543e6a4de94eb
bhanuj <b.bawdhane@accenture.com>
2023-09-05 15:08:28 +1000

    Refactored

--- src/components/slider/StoryBoardSlider.jsx
+++ src/components/slider/StoryBoardSlider.jsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { animatedCards } from '../../constants';
-import { slideVariants, staggerContainer } from '../../utils/motion';
+import { slideVariants } from '../../utils/motion';
 
 const StoryBoardSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [stopRotation, setStopRotation] = useState(false); // Add a state to control rotation
-
-  const finalVariant = {
-    opacity: 1
-  };
+  const [stopRotation, setStopRotation] = useState(false);
 
   const poster = {
     id: 'poster',
@@ -23,7 +19,6 @@ const StoryBoardSlider = () => {
       if (currentIndex < animatedCards.length - 1) {
         setCurrentIndex((prevIndex) => prevIndex + 1);
       } else {
-        // Stop the rotation when we reach the last image with id "poster"
         setStopRotation(true);
       }
     }, 2000);
@@ -33,49 +28,36 @@ const StoryBoardSlider = () => {
 
   return (
     <section className='slider-container'>
-      { !stopRotation && (
-          <AnimatePresence>
-            <motion.div
-              key={'test'}
-              variants={staggerContainer}
-              initial='hidden'
-              whileInView='show'
-              viewport={{ once: true }}
-            >
-              {animatedCards.map((image, index) => (
-                <div key={image.id}>
-                  <motion.img
-                    className='slider-image'
-                    src={image.imgUrl}
-                    alt={image.id}
-                    variants={slideVariants('right', 'left')}
-                    initial='initial'
-                    // Use a conditional to determine the animation for the last image
-                    animate={
-                      index === currentIndex
-                        ? stopRotation
-                          ? finalVariant // Apply the "final" variant to keep it static
-                          : 'animate' // Continue with the regular animation
-                        : ''
-                    }
-                    exit='exit'
-                  />
-                </div>
-              ))}
-            </motion.div>
-          </AnimatePresence>
-        )
-      }
+      <AnimatePresence>
+        { !stopRotation && (
+          animatedCards.map((image, index) => (
+            <div key={image.id}>
+              <motion.img
+                className='slider-image'
+                src={image.imgUrl}
+                alt={image.id}
+                variants={slideVariants('left', 'right')}
+                initial='initial'
+                animate={
+                  index === currentIndex
+                    ? 'animate'
+                    : ''
+                }
+                exit='exit'
+              />
+            </div>
+          ))
+        )}
 
-      { stopRotation && (
-        <div>
-          <img className="slider-image" href="#" src={poster.imgUrl} alt={poster.id}/>
-          <a className='story-poster-text' href='/'>
-            {poster.title}
-          </a>
-        </div>
-      )
-      }
+        { stopRotation && (
+          <div>
+            <img className="slider-image" href="#" src={poster.imgUrl} alt={poster.id}/>
+            <a className='story-poster-text' href='/'>
+              {poster.title}
+            </a>
+          </div>)
+        }
+      </AnimatePresence>
     </section>
   );
 };
--- src/utils/motion.js
+++ src/utils/motion.js
@@ -8,9 +8,27 @@ export const staggerContainer = (staggerChildren, delayChildren) => ({
     },
 });
 
+export const shineVariants = () => ({
+    initial:{ opacity: 0, width: '0%', height: '0%', x: '0%', y: '0%' },
+    animate: {
+        opacity: 1, 
+        width: '100%', 
+        height: '100%', 
+        x: '0px', 
+        y: '0px', 
+        transition: {
+          duration: 10, 
+          repeat: Infinity
+        }
+    }
+});
+
 export const slideVariants = (start, end) => ({
-    initial: { x: start === 'left' ? '-100%' : start === 'right' ? '100%' : 0 },
-    animate: { x: end === 'left' ? '-100vh' : end === 'right' ? '100vh' : 0 , transition: { duration: 3.5 } },
-    exit: { x: '-100vh', opacity: 0, transition: { duration: 10, ease: 'easeInOut' } },
-    final: { opacity: 1}
+    initial: { x: start === 'left' ? '-100%' : start === 'right' ? '100%' : 0, opacity: 1 },
+    animate: { 
+        x: end === 'left' ? '-100vh' : end === 'right' ? '100vh' : 0,
+        opacity: 1,
+        transition: { duration: 4, ease: 'easeOut' } 
+    },
+    exit: { opacity: 1 }
 });
