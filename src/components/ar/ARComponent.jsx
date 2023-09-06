32b948d402a398649c7e0a3bb4cb3ddeb84dc27c
bhanuj <b.bawdhane@accenture.com>
2023-09-07 08:46:27 +1000

    Modified the lines

--- src/components/ar/ARComponent.jsx
+++ src/components/ar/ARComponent.jsx
@@ -17,13 +17,17 @@ const ARComponent = () => {
     const {renderer, scene, camera} = mindarThree ? mindarThree : {};
    
     if (mindarThree) {
-        const anchor = mindarThree.addAnchor(0);
         const geometry = new THREE.PlaneGeometry(1, 0.55);
         const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
         const plane = new THREE.Mesh( geometry, material );
+        const anchor = mindarThree.addAnchor(0);
         anchor.group.add(plane);
     
-        mindarThree.start();
+        const start = async() => {
+          mindarThree.start();
+        }
+
+        start();
         
         if (renderer) {
             renderer.setAnimationLoop(() => {
@@ -45,64 +49,8 @@ const ARComponent = () => {
    }
   }, []);
 
-  const exampleTarget = document.querySelector('#example-target');
-  const examplePlane = document.querySelector('#example-plane');
-  const startButton = document.querySelector("#example-start-button");
-  const stopButton = document.querySelector("#example-stop-button");
-  const pauseButton = document.querySelector("#example-pause-button");
-  const pauseKeepVideoButton = document.querySelector("#example-pause-keep-video-button");
-  const unpauseButton = document.querySelector("#example-unpause-button");
-  
-  startButton && startButton.addEventListener('click', () => {
-    console.log("start");
-    containerRef.current.start(); // start AR 
-  });
-
-  stopButton && stopButton.addEventListener('click', () => {
-    containerRef.current.stop(); // stop AR 
-  });
-
-  pauseButton && pauseButton.addEventListener('click', () => {
-    containerRef.current.pause(); // pause AR, pause video
-  });
-
-  pauseKeepVideoButton && pauseKeepVideoButton.addEventListener('click', () => {
-    containerRef.current.pause(true); // pause AR, keep video
-  });
-
-  unpauseButton && unpauseButton.addEventListener('click', () => {
-    containerRef.current.unpause();
-  });
-   
-  if (exampleTarget) {
-    exampleTarget.addEventListener("targetFound", event => {
-      console.log("target found");
-    });
-    
-    exampleTarget.addEventListener("targetLost", event => {
-      console.log("target lost");
-    });
-  }
-
-  examplePlane && examplePlane.addEventListener("click", event => {
-    console.log("plane click");
-  });
-
-  return (    
-    <>
-      <div className="container">
-        <button id="example-start-button">Start</button>
-        <button id="example-pause-button">Pause</button>
-        <button id="example-pause-keep-video-button">Pause (keep video)</button>
-        <button id="example-unpause-button">UnPause</button>
-        <button id="example-stop-button">Stop</button>
-
-        <div className="container" style={{width: "100%", height: "100%"}} ref={containerRef}>
-          <div id="example-target" mindar-image-target="targetIndex: 0"></div>
-          <div id="example-plane" className="clickable" color="red"></div>
-        </div>
-      </div>
-    </>
+  return (
+    <div className="container" style={{width: "100%", height: "100%"}} ref={containerRef}></div>
   );
 };
 
