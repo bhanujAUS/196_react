38208b5469fe1762c43a4230dc09ea72ee3777d8
bhanuj <b.bawdhane@accenture.com>
2023-09-07 08:29:56 +1000

    Changes

--- src/components/ar/ARComponent.jsx
+++ src/components/ar/ARComponent.jsx
@@ -1,26 +1,48 @@
 import React, { useEffect, useRef } from 'react';
 
+import './styles.css'
+
+import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
+import * as THREE from 'three';
+
 const ARComponent = () => {
-  const sceneRef = useRef(null);
-  const arSystemRef = useRef(null);
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    const sceneEl = sceneRef.current;
-    let arSystem;
+    const mindarThree = new MindARThree({
+      container: containerRef.current,
+      imageTargetSrc: "./card.mind"
+    });
 
-    if (sceneEl) {
-      sceneEl.addEventListener('loaded', () => {
-        arSystem = sceneEl.systems["mindar-image-system"];
-        arSystemRef.current = arSystem;
-      });
-      
-      sceneEl.addEventListener("arReady", (event) => {
-        console.log("MindAR is ready")
-      });
-      sceneEl.addEventListener("arError", (event) => {
-        console.log("MindAR failed to start")
-      });
+    const {renderer, scene, camera} = mindarThree ? mindarThree : {};
+   
+    if (mindarThree) {
+        const anchor = mindarThree.addAnchor(0);
+        const geometry = new THREE.PlaneGeometry(1, 0.55);
+        const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
+        const plane = new THREE.Mesh( geometry, material );
+        anchor.group.add(plane);
+    
+        mindarThree.start();
+        
+        if (renderer) {
+            renderer.setAnimationLoop(() => {
+                renderer.render(scene, camera);
+              });
+        } else {
+            console.log("Renderer Absent");
+        }
+       
     }
+
+    return () => {
+      if (renderer) {
+          renderer.setAnimationLoop(null);
+      }
+      if (mindarThree && mindarThree !== 'undefined') {
+          //mindarThree.stop(); 
+      }
+   }
   }, []);
 
   const exampleTarget = document.querySelector('#example-target');
@@ -33,23 +55,23 @@ const ARComponent = () => {
   
   startButton && startButton.addEventListener('click', () => {
     console.log("start");
-    arSystemRef.current.start(); // start AR 
+    containerRef.current.start(); // start AR 
   });
 
   stopButton && stopButton.addEventListener('click', () => {
-    arSystemRef.current.stop(); // stop AR 
+    containerRef.current.stop(); // stop AR 
   });
 
   pauseButton && pauseButton.addEventListener('click', () => {
-    arSystemRef.current.pause(); // pause AR, pause video
+    containerRef.current.pause(); // pause AR, pause video
   });
 
   pauseKeepVideoButton && pauseKeepVideoButton.addEventListener('click', () => {
-    arSystemRef.current.pause(true); // pause AR, keep video
+    containerRef.current.pause(true); // pause AR, keep video
   });
 
   unpauseButton && unpauseButton.addEventListener('click', () => {
-    arSystemRef.current.unpause();
+    containerRef.current.unpause();
   });
    
   if (exampleTarget) {
@@ -68,24 +90,17 @@ const ARComponent = () => {
 
   return (    
     <>
-      <div>
+      <div className="container">
         <button id="example-start-button">Start</button>
         <button id="example-pause-button">Pause</button>
         <button id="example-pause-keep-video-button">Pause (keep video)</button>
         <button id="example-unpause-button">UnPause</button>
         <button id="example-stop-button">Stop</button>
-        <a-scene ref={sceneRef} 
-          mindar-image="imageTargetSrc: './things.mind'; autoStart: true;" 
-          color-space="sRGB" 
-          renderer="colorManagement: true, physicallyCorrectLights" 
-          vr-mode-ui="enabled: false" 
-          device-orientation-permission-ui="enabled: false"
-        >
-          <a-camera position="0 0 0" look-controls="enabled: false" cursor="fuse: false; rayOrigin: mouse;" raycaster="near: 10; far: 10000; objects: .clickable"></a-camera>
-          <a-entity id="example-target" mindar-image-target="targetIndex: 0">
-            <a-plane id="example-plane" className="clickable" color="blue" opacity="0.5" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-          </a-entity>
-        </a-scene>
+
+        <div className="container" style={{width: "100%", height: "100%"}} ref={containerRef}>
+          <div id="example-target" mindar-image-target="targetIndex: 0"></div>
+          <div id="example-plane" className="clickable" color="red"></div>
+        </div>
       </div>
     </>
   );
