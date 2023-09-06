95c9ba4dc9559670c1cc7feabda4244deee56e9e
He <katharine.he@accenture.com>
2023-09-06 13:36:35 +1000

    Style button to bottom

--- src/components/earth/Poster.jsx
+++ src/components/earth/Poster.jsx
@@ -27,8 +27,9 @@ export default function Poster() {
               animate={{ opacity: 1}}
               exit={{ opacity: 0 }}
               transition={{ opacity: 0.2 }}
+              style={{ position: "fixed", top: "0px"}}
             >
-              <img href="#" src={poster.imgUrl} alt={poster.id}/>
+              <img href="#" src={poster.imgUrl} alt={poster.id} style={{ width: '100%', height: '100vh', objectFit: 'cover' }} />
             </motion.div>
           </div>
         )}
